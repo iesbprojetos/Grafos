8c1fd914cdc5d9316a9c0beaf1ac4b4542b3aa55
Danilo Carvalho <dfcarvalho85@gmail.com>
2015-10-08 17:24:56 -0300

    Alterado:  - [Fix] Ao criar grafo, verifica se número de vértices é válido (maior que 0). Senão, mostrar alerta.
    O que falta:
     - Remover arcos com mouse?
     - Lidar com erros e exceptions

--- src/grafos/ui/NewGraphController.java
+++ src/grafos/ui/NewGraphController.java
@@ -2,11 +2,17 @@ package grafos.ui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -31,7 +37,32 @@ public class NewGraphController implements Initializable {
         String strV = textFieldVertices.getText();
         try {
             int v = Integer.parseInt(strV);
+            if (v <= 0) {
+                try {
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/res/alert_dialog.fxml"));
+                    Parent root = (Parent) fxmlLoader.load();
+
+                    AlertDialogController controller = fxmlLoader.getController();
+                    controller.setMessage("Número de vértices precisa ser maior que 0.");
+
+                    Scene scene = new Scene(root);
+
+                    Stage dialog = new Stage(StageStyle.UTILITY);
+                    dialog.initModality(Modality.WINDOW_MODAL);
+                    dialog.setTitle("Alerta");
+                    dialog.setScene(scene);
+                    dialog.sizeToScene();
+                    dialog.show();
+
+                    return;
+                } catch (IOException e) {
+                    // TODO:
+                    e.printStackTrace();
+                }
+            }
+
             drawingController.createGraph(v);
+
 
             Button src = (Button) event.getSource();
             Stage thisStage = (Stage) src.getScene().getWindow();
